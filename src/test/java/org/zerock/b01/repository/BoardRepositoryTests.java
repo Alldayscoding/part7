a43165b0106addafee1af9b5a5d8978a83f445cd
jason <jason.kim3221@gmail.com>
2023-08-13 19:27:04 -0400

    first commit

--- src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
+++ src/test/java/org/zerock/b01/repository/BoardRepositoryTests.java
@@ -32,7 +32,7 @@ public class BoardRepositoryTests {
 
     @Test
     public void testInsert() {
-        IntStream.rangeClosed(1,100).forEach(i -> {
+        IntStream.rangeClosed(1,30).forEach(i -> {
             Board board = Board.builder()
                     .title("title..." +i)
                     .content("content..." + i)
